c7f85db8bcd2a5dda8cb04adf7c9492920c1fe68
JucimarJunior <jucimar.vinente@hotmail.com>
2022-10-14 16:15:14 -0400

    update branchs

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 <
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
-@
-app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
 n
 >app/src/main/java/com/example/firebasekotlin/FirebaseConfig.kt,e/3/e335c3f5dd4f387e66d0ed20d2c027dd8dcb9954
 l
---
+++ app/src/main/java/com/example/firebasekotlin/ui/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.firebasekotlin.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.firebasekotlin.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
